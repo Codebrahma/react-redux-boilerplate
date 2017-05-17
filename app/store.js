967ffe7c5287bde02dc34d71a0906a00f921a369
prasanna1211 <prasanna.krystal@gmail.com>
2017-05-17 02:50:31 -0700

    added redux logger

--- app/store.js
+++ app/store.js
@@ -5,6 +5,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { fromJS } from 'immutable';
 import { routerMiddleware } from 'react-router-redux';
+import { createLogger } from 'redux-logger';
 import createReducer from './reducers';
 
 const devtools = window.devToolsExtension || (() => noop => noop);
@@ -12,19 +13,26 @@ const devtools = window.devToolsExtension || (() => noop => noop);
 export default function configureStore(initialState = {}, history) {
   // Create the store with middlewares
   // 1. routerMiddleware: Syncs the location/URL path to the state
+
+  // Logger for redux actions and actioncreators on console
+  const reduxLogger = createLogger({
+    stateTransformer: state => JSON.parse(JSON.stringify(state)),
+  });
+
   const middlewares = [
     routerMiddleware(history),
+    reduxLogger,
   ];
 
   const enhancers = [
     applyMiddleware(...middlewares),
-    devtools(),
   ];
 
   const store = createStore(
     createReducer(),
     fromJS(initialState),
-    compose(...enhancers)
+    compose(...enhancers),
+    devtools,
   );
 
   // Make reducers hot reloadable, see http://mxs.is/googmo
