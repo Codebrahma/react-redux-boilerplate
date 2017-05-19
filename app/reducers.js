e933368d4817a16c829f3c90c48f5350d9f40e7b
Anshul Sahni <anshul_sahni@live.com>
2017-05-19 11:37:27 +0530

    removed react-router-redux leftovers

--- app/reducers.js
+++ app/reducers.js
@@ -4,43 +4,12 @@
  */
 
 import { combineReducers } from 'redux-immutable';
-import { fromJS } from 'immutable';
-import { LOCATION_CHANGE } from 'react-router-redux';
-
-/*
- * routeReducer
- *
- * The reducer merges route location changes into our immutable state.
- * The change is necessitated by moving to react-router-redux@4
- *
- */
-
-// Initial routing state
-const routeInitialState = fromJS({
-  locationBeforeTransitions: null,
-});
-
-/**
- * Merge route into the global application state
- */
-function routeReducer(state = routeInitialState, action) {
-  switch (action.type) {
-    /* istanbul ignore next */
-    case LOCATION_CHANGE:
-      return state.merge({
-        locationBeforeTransitions: action.payload,
-      });
-    default:
-      return state;
-  }
-}
 
 /**
  * Creates the main reducer with the asynchronously loaded ones
  */
 export default function createReducer(asyncReducers) {
   return combineReducers({
-    route: routeReducer,
     ...asyncReducers,
   });
 }
